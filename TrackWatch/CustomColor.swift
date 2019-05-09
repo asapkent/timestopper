004c7bca702728bbfaa1bf8780d20f17d10386f2
asapkent <jeffersalex94@gmail.com>
2019-05-08 22:26:36 -0400

    Initial Commit

---
+++ TrackWatch/CustomColor.swift
@@ -0,0 +1,27 @@
+//
+//  CustomColor.swift
+//  TrackWatch
+//
+//  Created by Alex Jeffers on 5/1/19.
+//  Copyright © 2019 JeffWorld. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+    //UIColor is a website used to convert HEX & RGB colors to UIColor for both Objective-C and Swift featuring a colorpicker and copy to clipboard functionality making things easier. http://uicolor.xyz/#/hex-to-ui
+    
+    let flipside = UIColor(red:0.09, green:0.10, blue:0.11, alpha:1.0)
+    let aquaBlue = UIColor(red:0.00, green:0.50, blue:1.00, alpha:1.0)
+    let snow = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+    let tungsten = UIColor(red:0.20, green:0.20, blue:0.20, alpha:1.0)
+    let strawberry = UIColor(red:0.999, green:0.184, blue:0.572, alpha:1.0)
+    let blackTranslucent = UIColor(red:0.00, green:0.00, blue:0.00, alpha:0.1)
+    let black = UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0)
+    let red = UIColor(red:1.00, green:0.00, blue:0.00, alpha:1.0)
+    let groupTable = UIColor(red:0.92, green:0.92, blue:0.95, alpha:1.0)
+    
+    let lime = UIColor(red:0.00, green:1.00, blue:0.00, alpha:1.0)
+    let lemon = UIColor(red:1.00, green:1.00, blue:0.00, alpha:1.0)
+    let summerSky = UIColor(red:0.00, green:0.68, blue:1.00, alpha:1.0)
+
---
+++ TrackWatch/TrackWatchViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  TrackWatch
+//
+//  Created by Alex Jeffers on 5/1/19.
+//  Copyright © 2019 JeffWorld. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackWatchViewController: UIViewController {
+
+    @IBOutlet weak var restButton: UIButton!
+    @IBOutlet weak var timeKeeper: UILabel!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var timer = Timer()
+    var istimerRunning = false
+    var counter = 0.0
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+//        startButton.backgroundColor = blackTranslucent
+//        pauseButton.backgroundColor = blackTranslucent
+//        restButton.backgroundColor = blackTranslucent
+        view.backgroundColor = black
+        //timeKeeper.backgroundColor = snow
+        restButton.isEnabled = false
+        pauseButton.isEnabled = false
+        startButton.isEnabled = true
+    }
+
+
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+//        if startButton.backgroundColor == blackTranslucent {
+//            startButton.backgroundColor = lime
+//        } else  if
+//            startButton.backgroundColor == lime {
+//                startButton.backgroundColor = blackTranslucent
+//            }
+        if !istimerRunning {
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(runTimer), userInfo: nil, repeats: true)
+           
+            istimerRunning = true
+            restButton.isEnabled = true
+            pauseButton.isEnabled = true
+            startButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func pauseButtonPressed(_ sender: UIButton) {
+//        if pauseButton.backgroundColor == blackTranslucent {
+//            pauseButton.backgroundColor = red
+//        } else if
+//            pauseButton.backgroundColor == red {
+//            pauseButton.backgroundColor = blackTranslucent
+//        }
+            restButton.isEnabled = true
+            startButton.isEnabled = true
+            pauseButton.isEnabled = false
+        
+            istimerRunning = false
+            timer.invalidate()
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        istimerRunning = false
+        counter = 0.0
+        
+        timeKeeper.text = "00:00:00.0"
+        
+        restButton.isEnabled = false
+        pauseButton.isEnabled = false
+        startButton.isEnabled = true
+    }
+    
+    @objc func runTimer()  {
+      counter += 0.1
+        let baseCounter = Int(floor(counter))
+        let hour = baseCounter / 3600
+        let minute = (baseCounter % 3600 ) / 60
+        var minuteString = "\(minute)"
+        if minute < 10 {
+            minuteString = "0\(minute)"
+        }
+        
+        let second = (baseCounter % 3600) % 60
+        var secondString = "\(second)"
+        if second < 10 {
+            secondString = "0\(second)"
+        }
+        //timeKeeper.text = "\(counter)"
+        let deSecond = String(format: "%.1f", counter).components(separatedBy: ".").last!
+        
+        timeKeeper.text = "\(hour):\(minuteString):\(secondString).\(deSecond)"
+    }
+}
+
+
